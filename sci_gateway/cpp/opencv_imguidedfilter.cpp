80785dc43fe8cdf8353fadda3b60dbfdd0865065
yashbhalgat <yashbhalgat95@gmail.com>
2015-12-18 06:27:56 +0530

    Guided filter working for all cases - RGB and grayscale - and all usages of MATLAB

--- sci_gateway/cpp/opencv_imguidedfilter.cpp
+++ sci_gateway/cpp/opencv_imguidedfilter.cpp
@@ -6,6 +6,8 @@ References : Kaiming He, Jian Sun, Xiaoou Tang, "Guided Image Filtering", IEEE T
 			pp. 1397-1409, June 2013, doi:10.1109/TPAMI.2012.213
 ***************************************************************************************
 Usage :
+	Covers all the cases of I and p being grayscale and RGB
+
 	1) guidedFiltered_image = imguidedfilter(input_img);
 		In this usage, the image itself is used as the guidance image.
 	
@@ -18,7 +20,7 @@ Usage :
 Example : 
 	img = imread("lena.jpg");
 	imshow(img);
-	guidedFiltered_image = imguidedfilter(img, 9, 9);
+	guidedFiltered_image = imguidedfilter(img, img, 9);
 	imshow(guidedFiltered_image);
 ***********************************************************************/
 
@@ -110,35 +112,101 @@ extern "C"
 
     //Main function
     //if both filter_size is not given, default value of 3x3 is used
-    Mat mean_p;
-    blur(p, mean_p, Size(r, r));
-    
-    Mat mean_Ip;
-    blur(I.mul(p), mean_Ip, Size(r, r));
-    
-    Mat mean_I;
-    blur(I, mean_I, Size(r, r));
-    
-    Mat mean_II;
-    blur(I.mul(I), mean_II, Size(r,r));
-    
-    Mat var_I = mean_II - mean_I.mul(mean_I);
-    
-    Mat cov_Ip = mean_Ip - mean_I.mul(mean_p); //covariance of (I, p) in each local patch.
+	if(I.channels() == 1){
+		Mat mean_p;
+		blur(p, mean_p, Size(r, r));
+		
+		Mat mean_Ip;
+		blur(I.mul(p), mean_Ip, Size(r, r));
+		
+		Mat mean_I;
+		blur(I, mean_I, Size(r, r));
+		
+		Mat mean_II;
+		blur(I.mul(I), mean_II, Size(r,r));
+		
+		Mat var_I = mean_II - mean_I.mul(mean_I);
+		
+		Mat cov_Ip = mean_Ip - mean_I.mul(mean_p); //covariance of (I, p) in each local patch.
 
-    Mat a = cov_Ip / (var_I + eps);
-    Mat b = mean_p - a.mul(mean_I);
+		Mat a = cov_Ip / (var_I + eps);
+		Mat b = mean_p - a.mul(mean_I);
 
-    Mat mean_a;
-    blur(a, mean_a, Size(r, r));
-    Mat mean_b;
-    blur(b, mean_b, Size(r, r));
+		Mat mean_a;
+		blur(a, mean_a, Size(r, r));
+		Mat mean_b;
+		blur(b, mean_b, Size(r, r));
 
-    guidedFiltered_image = mean_a.mul(I) + mean_b;
-    
-    
-    
-    
+		guidedFiltered_image = mean_a.mul(I) + mean_b;
+    }
+    else if(I.channels()==3 && p.channels()==1){
+		vector<cv::Mat> Ichannels, channels;
+		split(I, Ichannels);
+		guidedFiltered_image = I;
+		//split(guidedFiltered_image, channels);
+		
+		Mat mean_p, mean_I, mean_Ip;
+		Mat mean_II, var_I, cov_Ip, a, b;
+		Mat mean_a, mean_b;
+
+		for(i=0; i<3; i++){
+			blur(p, mean_p, Size(r, r));
+			
+			blur(Ichannels[i].mul(p), mean_Ip, Size(r, r));
+			
+			blur(Ichannels[i], mean_I, Size(r, r));
+			
+			blur(Ichannels[i].mul(Ichannels[i]), mean_II, Size(r,r));
+			
+			var_I = mean_II - mean_I.mul(mean_I);
+			
+			cov_Ip = mean_Ip - mean_I.mul(mean_p); //covariance of (Ichannels[i], p) in each local patch.
+
+			a = cov_Ip / (var_I + eps);
+			b = mean_p - a.mul(mean_I);
+
+			blur(a, mean_a, Size(r, r));
+			blur(b, mean_b, Size(r, r));
+
+			channels.push_back(mean_a.mul(Ichannels[i]) + mean_b);
+		}
+		merge(channels,guidedFiltered_image);
+	}
+	else if(I.channels()==3 && p.channels()==3){
+		vector<cv::Mat> Ichannels, channels, pchannels;
+		
+		split(I, Ichannels);
+		guidedFiltered_image = I;
+		//split(guidedFiltered_image, channels);
+		split(p, pchannels);
+		
+		Mat mean_p, mean_I, mean_Ip;
+		Mat mean_II, var_I, cov_Ip, a, b;
+		Mat mean_a, mean_b;
+
+		for(i=0; i<3; i++){
+			blur(pchannels[i], mean_p, Size(r, r));
+			
+			blur(Ichannels[i].mul(pchannels[i]), mean_Ip, Size(r, r));
+			
+			blur(Ichannels[i], mean_I, Size(r, r));
+			
+			blur(Ichannels[i].mul(Ichannels[i]), mean_II, Size(r,r));
+			
+			var_I = mean_II - mean_I.mul(mean_I);
+			
+			cov_Ip = mean_Ip - mean_I.mul(mean_p); //covariance of (Ichannels[i], pchannels[i]) in each local patch.
+
+			a = cov_Ip / (var_I + eps);
+			b = mean_p - a.mul(mean_I);
+
+			blur(a, mean_a, Size(r, r));
+			blur(b, mean_b, Size(r, r));
+
+			channels.push_back(mean_a.mul(Ichannels[i]) + mean_b);
+		}
+		merge(channels,guidedFiltered_image);
+	}
 
     //returning image
     string tempstring = type2str(guidedFiltered_image.type());
